9cc8811a774ff9f7464fa4a85bef271d1373c042
ManhThang98er <papaboiboi@gmail.com>
2022-03-15 19:35:32 +0700

    add b7.c file

---
+++ b7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    float S=0;
+    printf("nhap n= ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        S=S+(i*1.0)/(i+1);
+    }
+    printf("S= %f",S);
+    return 0;
+}
