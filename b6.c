f17d64d31b1143847a98d09c227d4439945620d4
ManhThang98er <papaboiboi@gmail.com>
2022-03-15 19:31:10 +0700

    add b.c file

---
+++ b6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    float S=0;
+    printf("nhap n= ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++)
+    {
+        S=S+1.0/(i*(i+1));
+    }
+    printf("S= %f",S);
+    return 0;
+}
